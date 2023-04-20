bf35258b1c889db9717a78684271b8b6c88a236c
Bernardo Mantoan Castellani <bernardo.castellani@alunos.bpkedu.com.br>
2023-04-19 22:02:15 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './index.css'
+import { Button, Box, FormControl } from '@mui/material';
 
 function App() {
+  const [time, setTime] = useState('');
+  const [distance, setDistance] = useState('');
+  const [result, setResult] = useState('');
+
+  const handleTimeChange = (event) => {
+    setTime(event.target.value);
+  }
+
+  const handleDistanceChange = (event) => {
+    setDistance(event.target.value);
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const flashTime = (distance / 1100) * 60;
+    const resultTime = flashTime.toFixed(2);
+    setResult(`O Flash faria sua atividade em ${resultTime} minutos`);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <link href="https://fonts.googleapis.com/css?family=Sen&display=swap" rel="stylesheet"></link>
+      <div className='container'>
+      <div className='formulario'>
+        <FormControl component="form" onSubmit={handleSubmit}>
+          <Box>
+            <label>
+              Tempo de sua Corrida/Atividade (minutos): </label><br></br>
+              <input type="number" className="inputTempo"value={time} onChange={handleTimeChange} />
+            <br />
+            <label className='distlabel'>
+              Distancia (quilômetros): </label> <br></br>
+              <input type="number" className="inputDistancia" value={distance} onChange={handleDistanceChange} />
+          </Box>
+        
+          <br />
+          <Button type="submit">Converter</Button>
+        </FormControl>
+        </div>
+        <div className='resultadoButton'>
+        <p>{result}</p>
+        </div>
+        </div>
+      
     </div>
   );
 }
